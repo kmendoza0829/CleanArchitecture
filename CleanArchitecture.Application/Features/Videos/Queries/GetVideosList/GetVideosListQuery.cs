b874a394435ae6f6391405ae3d687c5074a31d79
Kevin Mendoza <kevin.mendoza@masiv.com>
2022-03-06 22:22:29 -0500

    Implementando CQRS y MediatR

---
+++ CleanArchitecture.Application/Features/Videos/Queries/GetVideosList/GetVideosListQuery.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Application.Features.Videos.Queries.GetVideosList
+{
+    public class GetVideosListQuery : IRequest<List<VideosVm>>
+    {
+        public string Username { get; set; } = string.Empty;
+
+        public GetVideosListQuery(string? username)
+        {
+            Username = username ?? throw new ArgumentNullException(nameof(username));
+        }
+    }
+}
